069b608d3c6d900e55f57b132ee1d6e43cd3eb6a
Krzysztof Kuhn <kuhn.krzysztof@gmail.com>
2023-09-17 13:58:52 +0200

    Fixed updating order with new order items

--- TastyRestaurant.WebApi/Infrastructure/Repositories/OrderRepository.cs
+++ TastyRestaurant.WebApi/Infrastructure/Repositories/OrderRepository.cs
@@ -37,7 +37,31 @@ public class OrderRepository : IOrderRepository
 
     public async Task UpdateAsync(Order order)
     {
-        _dbContext.Orders.Update(order);
-        await _dbContext.SaveChangesAsync();
+        _dbContext.Update(order);
+
+        var saved = false;
+        while (!saved)
+        {
+            try
+            {
+                // Attempt to save changes to the database
+                await _dbContext.SaveChangesAsync();
+                saved = true;
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                foreach (var entry in ex.Entries)
+                {
+                    if (entry.Entity is OrderItem)
+                    {
+                        _dbContext.Entry(entry.Entity).State = EntityState.Added;
+                    }
+                    else
+                    {
+                        throw new NotSupportedException($"Don't know how to handle concurrency conflicts for {entry.Metadata.Name}");
+                    }
+                }
+            }
+        }
     }
 }
